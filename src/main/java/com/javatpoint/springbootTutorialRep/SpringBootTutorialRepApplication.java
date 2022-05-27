75484d87725ce4d9f4d19819925bd1ad0166fe9e
AidinDeveloper <aidin.masroor@gmail.com>
2022-05-27 18:05:24 +0400

    Spring Boot Initialization

---
+++ src/main/java/com/javatpoint/springbootTutorialRep/SpringBootTutorialRepApplication.java
@@ -0,0 +1,13 @@
+package com.javatpoint.springbootTutorialRep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTutorialRepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTutorialRepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatpoint/springbootTutorialRep/SpringBootTutorialRepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatpoint.springbootTutorialRep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialRepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
